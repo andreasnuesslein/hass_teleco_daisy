36a6bdb3cf78212a751591829ae5c3f968b42389
Andreas Nüßlein <andreas@nuessle.in>
2025-09-01 09:46:15 +0100

    improvements to the covers, more downwards compatibility. v0.1.8

--- custom_components/teleco_daisy/cover.py
+++ custom_components/teleco_daisy/cover.py
@@ -16,7 +16,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.device_registry import DeviceInfo
 
 from .const import DOMAIN
-from teleco_daisy import DaisyAwningsCover, DaisySlatsCover
+from teleco_daisy import DaisyAwningsCover, DaisySlatsCover, DaisyShadeCover
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -31,7 +31,9 @@ async def async_setup_entry(
 
 
 class TelecoDaisyCover(CoverEntity):
-    def __init__(self, cover: DaisyAwningsCover | DaisySlatsCover) -> None:
+    def __init__(
+        self, cover: DaisyAwningsCover | DaisySlatsCover | DaisyShadeCover
+    ) -> None:
         self._cover = cover
 
         self._attr_unique_id = str(cover.idInstallationDevice)
@@ -52,6 +54,13 @@ class TelecoDaisyCover(CoverEntity):
                 | CoverEntityFeature.CLOSE_TILT
                 | CoverEntityFeature.SET_TILT_POSITION
                 | CoverEntityFeature.STOP_TILT
+            )
+        elif isinstance(cover, DaisyShadeCover):
+            self._attr_device_class = CoverDeviceClass.SHADE
+            self._attr_supported_features = (
+                CoverEntityFeature.OPEN
+                | CoverEntityFeature.CLOSE
+                | CoverEntityFeature.STOP
             )
 
     @property
@@ -74,6 +83,11 @@ class TelecoDaisyCover(CoverEntity):
     def current_cover_tilt_position(self) -> int | None:
         return self._cover.position
 
+    def update(self) -> None:
+        self._cover.update_state()
+        self._attr_is_closed = self._cover.is_closed
+        self._attr_current_cover_position = self._cover.position
+
     # @property
     # def is_closing(self) -> bool:
     #     """Return if the cover is closing or not."""
@@ -84,6 +98,7 @@ class TelecoDaisyCover(CoverEntity):
     #     """Return if the cover is opening or not."""
     #     return self._roller.moving > 0
     #
+
     def open_cover(self, **kwargs: Any) -> None:
         self._cover.open_cover()
         self.update()
@@ -109,10 +124,12 @@ class TelecoDaisyCover(CoverEntity):
         self.update()
 
     def open_cover_tilt(self, **kwargs: Any) -> None:
-        self.open_cover(**kwargs)
+        self._cover.open_cover()
+        self.update()
 
     def close_cover_tilt(self, **kwargs: Any) -> None:
-        self.close_cover(**kwargs)
+        self._cover.close_cover()
+        self.update()
 
     def set_cover_tilt_position(self, **kwargs: Any) -> None:
         position = kwargs[ATTR_TILT_POSITION]
@@ -127,9 +144,5 @@ class TelecoDaisyCover(CoverEntity):
         self.update()
 
     def stop_cover_tilt(self, **kwargs: Any) -> None:
-        self.stop_cover(**kwargs)
-
-    def update(self) -> None:
-        self._cover.update_state()
-        self._attr_is_closed = self._cover.is_closed
-        self._attr_current_cover_position = self._cover.position
+        self._cover.stop_cover()
+        self.update()
